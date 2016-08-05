f27dbf893690169020d7aef91c582027832c1591
val-ravn <krystof.riha@outlook.com>
2016-08-06 00:12:00 +0200

    Remove ping-pong, add ready event and graceful exit
    Also consistent quotes

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 //Load packages
-var config = require('config');
-var Discord = require('discord.js');
+var config = require("config");
+var Discord = require("discord.js");
 
 //Load config
-var discordToken = config.get('Discord.token');
+var discordToken = config.get("Discord.token");
 
 var bot = new Discord.Client();
 
-bot.on("message", function(msg) {
-    if (msg.content === "ping") {
-        bot.reply(msg, "pong");
-    }
+bot.on("ready", function() {
+    console.log(bot.user.name + " (ID: " + bot.user.id + ") is online");
 });
 
 bot.loginWithToken(discordToken);
+
+//Allow the bot to cleanup before terminating the process
+process.on("exit", function() {
+    exitGracefully();
+});
+
+process.on("SIGINT", function() {
+    exitGracefully();
+});
+
+//Completely log out the bot and exit the process
+function exitGracefully() {
+    bot.destroy();
+    process.exit(-1);
+}
