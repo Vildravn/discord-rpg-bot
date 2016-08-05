b89834a44c3519e5885e4c9ce7373d9ca88425d4
val-ravn <krystof.riha@outlook.com>
2016-08-06 00:27:24 +0200

    Add a basic framework for commands

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ var Discord = require("discord.js");
 
 //Load config
 var discordToken = config.get("Discord.token");
+var ownerId = config.get("Discord.ownerID");
+var msgPrefix = config.get("Discord.prefix");
 
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
     console.log(bot.user.name + " (ID: " + bot.user.id + ") is online");
+});
+
+bot.on("message", function(msg) {
+    //If a received message starts with a prefix, treat it as a command
+    if (msg.content.startsWith(msgPrefix)) {
+        if (msg.author.id == ownerId) {
+            //Below are owner-exclusive commands
+        } else {
+            //Below are user commands
+        }
+    }
 });
 
 bot.loginWithToken(discordToken);
